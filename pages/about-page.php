43add6a6f5ad0d97fe075f9b4aafaeb28bdd2346
Your Name <you@example.com>
2019-11-02 12:47:03 -0700

    First Upload

---
+++ pages/about-page.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Phone Directory</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+                    <!----- Nav bar Start ----->
+    <div class="container-fluid bg-dark">
+        <div class="container">
+            <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top justify-content-center">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link" href="../index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link " href="contact-page.php">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-white" href="about-page.php">About Us</a>
+                    </li>
+                </ul>
+            </nav>
+        
+        </div> <!-- container end -->
+    </div><!-- container fluid end -->
+
+
+                    <!------- Search bar start------->
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="input-group mt-5 w-50 mx-auto">
+                        <input type="text" class="form-control bg-dark pt-3 pb-3 w-25 text-white" placeholder="Search...">
+                        <div class="input-group-append">
+                        <button class="btn btn-secondary bg-white" type="button">
+                            <i class="fa fa-search text-dark"></i>
+                        </button>
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
+        </div><!-- container end -->
+    </div> <!-- container fluid end -->
+                    <!------- Search bar end ------->
+
+
+
+                    <!------- Button List Start ------->
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-md-12 text-center mt-5">
+                    <p class="h2 mb-4 text-dark">Are you searching like?</p>
+                    <button type="button" class="btn btn-lg btn-outline-primary">Primary</button>
+                    <button type="button" class="btn btn-lg btn-outline-secondary">Secondary</button>
+                    <button type="button" class="btn btn-lg btn-outline-dark">Dark</button>
+                    <button type="button" class="btn btn-lg btn-outline-primary">Primary</button>
+                    <button type="button" class="btn btn-lg btn-outline-secondary">Secondary</button>
+                    <button type="button" class="btn btn-lg btn-outline-dark">Dark</button>
+                
+                </div>
+            
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center mt-3">
+                    <button type="button" class="btn btn-lg btn-outline-primary">Primary</button>
+                    <button type="button" class="btn btn-lg btn-outline-secondary">Secondary</button>
+                    <button type="button" class="btn btn-lg btn-outline-dark">Dark</button>
+                    <button type="button" class="btn btn-lg btn-outline-primary">Primary</button>
+                    <button type="button" class="btn btn-lg btn-outline-secondary">Secondary</button>
+                    <button type="button" class="btn btn-lg btn-outline-dark">Dark</button>
+                
+                </div>
+            
+            </div> <!-- Row end -->
+        
+        </div><!-- container end -->
+    </div> <!-- container fluid end -->
+                    <!------- Button List End ------->
+
+
+
+                    <!------- Footer Start ------->
+    <div class="container-fluid bg-dark footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                            <a class="nav-link" href="../index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="contact-page.php">Contact Us</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link text-white" href="about-page.php">About Us</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    
+                
+                </div>
+                <div class="col-md-6 mt-3">
+                    <p class="text-white"> &copy;Copyright 2016 - Nuruddin Ripon. All rights reserved. </p>
+                    
+                
+                </div>
+            
+            </div>
+
+        </div>  <!-- container end -->
+    
+    </div>  <!-- container fluid end -->
+                     <!------- Footer End ------->
+</body>
+</html>
